88537b599fe30d5317d7216ba5f294b466e746c1
Selvaraj59 <rajabvijay@gmail.com>
2017-05-05 07:45:32 +0530

    test

---
+++ TestService/TestService/IService1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TestService
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        int Add(int value1, int value2);
+    }
+}
---
+++ TestService/TestService/Service1.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TestService
+{
+    public class Service1 : IService1
+    {
+        public int Add(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+    }
+}
